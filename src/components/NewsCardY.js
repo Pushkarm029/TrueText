4df8cccab63e1c8a14ba7aae7c2fec3ca62162fb
ayush-gupta-dev <ayushguptadev1@gmail.com>
2023-10-19 23:24:47 +0530

    news screen completed

--- src/components/NewsCardY.js
+++ src/components/NewsCardY.js
@@ -1,11 +1,29 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Text, TouchableOpacity, Image, Linking} from 'react-native';
 
-const NewsCardY = () => {
+const NewsCardY = ({title, link, image_url}) => {
+  const handlePress = () => {
+    // Open the external link when the card is clicked.
+    Linking.openURL(link); // Make sure to import Linking and request the necessary permissions to open URLs.
+  };
+
   return (
-    <View className="bg-slate-400 h-32 w-40 m-3 p-3 ">
-      <Text>NewsCardY</Text>
-    </View>
+    <TouchableOpacity onPress={handlePress}>
+      <View className="bg-black w-full my-2 mx-2">
+        <Image
+          source={
+            image_url ? {uri: image_url} : require('../assets/DefaultNews.jpg')
+          }
+          className="h-40 w-full opacity-50"
+        />
+
+        <Text
+          style={{textAlignVertical: 'center'}}
+          className="text-white drop-shadow-xl p-7 mx-auto font-semibold left-0 w-42 absolute h-32 text-center">
+          {title}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- src/screens/News.js
+++ src/screens/News.js
@@ -13,34 +13,77 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import axios from 'axios';
 
 const API_KEY = 'pub_31460f6837fbe6e5985d855312f8f03faf51b';
+const API_KEY_newsapi = '9b954e249b4c4717b68f72e8f14c4f15';
 
 const News = () => {
   const [trendingNews, settrendingNews] = useState([]);
+  const [scamNews, setscamNews] = useState([]);
+  const [latestNews, setlatestNews] = useState([]);
 
-  const fetchNews = async category => {
+  const fetchTrendNews = async () => {
     try {
       const response = await axios.get(`https://newsdata.io/api/1/news`, {
         params: {
           apikey: API_KEY,
-          q: category,
+          q: 'trending',
           country: 'in',
           language: 'en',
           image: 1,
         },
       });
       settrendingNews(response.data.results);
-      console.log(response.data.results[0].article_id);
+      //  console.log(response.data.results[0].article_id);
     } catch (error) {
       console.log('err', error);
     }
   };
+  const fetchScamNews = async () => {
+    try {
+      const response = await axios.get(`https://newsdata.io/api/1/news`, {
+        params: {
+          apikey: API_KEY,
+          q: 'online scam',
+          country: 'in',
+          language: 'en',
+          image: 1,
+        },
+      });
+      setscamNews(response.data.results);
+      console.log(response.data.results[0].title, response.data.totalResults);
+    } catch (error) {
+      console.log('err', error);
+    }
+  };
+  const fetchLatestNews = async () => {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines`, {
+        params: {
+          apikey: API_KEY_newsapi,
+          country: 'in',
+        },
+      });
+      setlatestNews(response.data.articles);
+      console.log(
+        'newsorg',
+        response.data.articles[0].title,
+        response.data.totalResults,
+      );
+    } catch (error) {
+      console.log('err', error);
+    }
+  };
+
   useEffect(() => {
-    fetchNews('breaking');
+    fetchTrendNews();
+    fetchScamNews();
+    fetchLatestNews();
   }, []);
+
+  //refresh on pull
   const [refreshing, setRefreshing] = useState(false);
   const onRefresh = async () => {
     setRefreshing(true);
-    fetchNews('breaking');
+    fetchTrendNews();
     setRefreshing(false);
   };
 
@@ -50,7 +93,7 @@ const News = () => {
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }>
       <SafeAreaView>
-        <View className="flex flex-row m-2">
+        <View className="flex flex-row mx-2 mt-2">
           <Text className="text-xl  font-bold text-blue-800">Trending</Text>
           <View className="justify-end pb-[1px] pl-[1px]">
             <FontAwesome5 name="fire" size={20} color={'#1434A4'} />
@@ -71,25 +114,36 @@ const News = () => {
         />
       </SafeAreaView>
       <SafeAreaView>
-        <Text className="text-xl font-bold text-red-800 m-2">Scams!</Text>
-        <ScrollView horizontal={true} className="m-1"></ScrollView>
+        <Text className="text-xl font-bold text-red-800 mx-2">Scams!</Text>
+        <FlatList
+          className="m-1"
+          horizontal={true}
+          data={scamNews}
+          keyExtractor={item => item.article_id.toString()}
+          renderItem={({item}) => (
+            <NewsCardX
+              title={item.title}
+              link={item.link}
+              image_url={item.image_url}
+            />
+          )}
+        />
       </SafeAreaView>
-
-      <Text className="text-lg font-bold text-blue-800 mx-2"> Latest </Text>
-      <View className="flex flex-row flex-wrap mx-2">
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-        <NewsCardY />
-      </View>
+      <SafeAreaView>
+        <Text className="text-lg font-bold text-blue-800 mx-2"> Latest </Text>
+        <FlatList
+          className="m-1"
+          data={latestNews}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => (
+            <NewsCardY
+              title={item.title}
+              link={item.url}
+              image_url={item.urlToImage}
+            />
+          )}
+        />
+      </SafeAreaView>
     </ScrollView>
   );
 };
