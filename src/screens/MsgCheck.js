09a6b5332e5df6bd952c59cd303d1acad368aaec
ayush-gupta-dev <ayushguptadev1@gmail.com>
2023-10-19 15:51:00 +0530

    textBox bug solved, im god

--- src/screens/MsgCheck.js
+++ src/screens/MsgCheck.js
@@ -1,12 +1,35 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
 
 const MsgCheck = () => {
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState('');
+
   return (
-    <View>
-      <Text>MsgCheck</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default MsgCheck;
